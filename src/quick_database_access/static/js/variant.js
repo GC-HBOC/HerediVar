4eb0328f0f10482884e1a39f930c355aed7d822e
MarvinDo <qarst@gmx.de>
2022-05-03 09:18:48 +0200

    code refactoring & bugfixes

--- src/quick_database_access/static/js/variant.js
+++ src/quick_database_access/static/js/variant.js
@@ -1,126 +1,17 @@
-/////////////// The following filters and sort the variant consequence table such that the tabs ensembl and refseq are working!
 
-filterTable_one_column("ensembl", 11)
-
-function filterTable_one_column(filter, col) {
-    var table, tr, td, cell, i;
-    filter = filter.toUpperCase();
-    table = document.getElementById("variantConsequenceTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 1; i < tr.length; i++) { // loop over rows
-        tr[i].style.display = "none"; // hide row
-    
-        td = tr[i].getElementsByTagName("td");
-        if (col <= td.length && col >= 0) {
-            cell = tr[i].getElementsByTagName("td")[col];
-            if (cell) {
-                if (cell.innerText.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                }
-            }
-        }
-    }
-    sortTable()
-}
-
-// not used atm!
-function filterTable_all_columns(filter) {
-    var table, tr, td, cell, i, j;
-    filter = filter.toUpperCase();
-    table = document.getElementById("variantConsequenceTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 1; i < tr.length; i++) {
-        tr[i].style.display = "none"; // hide row
-    
-        td = tr[i].getElementsByTagName("td");
-        for (var j = 0; j < td.length; j++) {
-            cell = tr[i].getElementsByTagName("td")[j];
-            if (cell) {
-                if (cell.innerText.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                break;
-                }
-            }
-        }
-    }
-    sortTable()
-}
-
-
-function sortTable(){
+table = document.getElementById("variantConsequenceTable");
+if (table != null) {
+    filterTable_one_column("ensembl", 11, table);
     sorter(['#variant_consequence_numflags_col', '#variant_consequence_length_col']) // sort first by num of flags and if there is a tie sort by length
 }
-
-function sorter(colids){ // !! only insert columns from the same table
-    var indices = []
-    for (var i = 0; i < colids.length; i++) {
-        var domid = colids[i]
-        indices.push($(domid).index())
-    }
-    var table = $(domid).parents('table').eq(0)
-    var rows = table.find('tr:gt(0)').toArray().sort(comparer(indices), )
-    rows = rows.reverse()
-    for (var i = 0; i < rows.length; i++){
-        table.append(rows[i])
-    }
-}
-
-function comparer(indices) {
-    return function(a, b) {
-        for (var i = 0; i < indices.length; i++) {
-            var row_index = indices[i]
-            var valA = getCellValue(a, row_index)
-            var valB = getCellValue(b, row_index)
-            var result = sort_helper(valA, valB)
-            if (result !== 0) {
-                break;
-            }
-        }
-
-        return result
-
-    }
-}
-
-function sort_helper(valA, valB){
-    // sort none at the end
-    var exceptions = [ "none" ];
-    if (exceptions.includes(valA.toString().toLowerCase())) {
-        return -1
-    }
-    if (exceptions.includes(valB.toString().toLowerCase())) {
-        return 1
-    }
-    
-    //normal case
-    if ($.isNumeric(valA) && $.isNumeric(valB)) { 
-        return valA - valB
-    } else {
-        return valA.toString().localeCompare(valB)
-    }
-}
-
-function getCellValue(row, index){
-    return $(row).children('td').eq(index).text()
-}
-
 
 
 
 
 $(document).ready(function()
 {
-    ////////// activate bootstrap tooltips
-    $("body").tooltip({ selector: '[data-bs-toggle=tooltip]' });
-
-    ////////// functionality for the reannotate button
-    $('#reannotate_button').click(function()
-    {
-        $(this).attr('disabled', true);
-        return true;
-    });
-
     var annotation_status = $('#annotation_status').data()['annotationStatus'];
+    console.log(annotation_status)
     if (annotation_status === 'pending'){
         $('#reannotate_button').attr('disabled', true)
         $('#tooltip_reannotate_button').attr('title', "Wait for the current annotation to finish before submitting another one")
