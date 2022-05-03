4fdecefcadd03324ff8b7c132a652eaf13301d9a
MarvinDo <qarst@gmx.de>
2022-05-03 09:18:06 +0200

    update arup brca parser (#2)

--- src/tools/db_converter_arup.py
+++ src/tools/db_converter_arup.py
@@ -25,6 +25,14 @@ reference_transcript = args.reference
 
 print('#reference\thgvs_c\tclassification')
 
+
+# this script tranforms:
+# c.311_312delGCins9
+# c.244_245insA -> c.244_245ins
+# c.38_39delATinsGGG -> c.38_39delinsGGG
+
+
+
 first_line = True
 cdot  = ''
 clsf = ''
@@ -38,6 +46,15 @@ for line in input_file:
         first_line = False
     if line.startswith('<td class="cDot">'):
         cdot = re.search('cDot">(.*)[<]', line).group(1)
+        matches_deletion_insertions = re.search('(.*del).*(ins.*)', cdot)
+        if matches_deletion_insertions is not None: # check if it is an insertion deletion and remove bases between them
+            cdot = matches_deletion_insertions.group(1) + matches_deletion_insertions.group(2)
+        else: 
+            matches = re.search('dup|ins|del|inv', cdot)
+            if matches is not None: # check if it is an insertion duplication or deletion and remove bases preceding these keywords
+                cut_here = matches.end(0)
+                cdot = cdot[:cut_here]
+        # don't do anything if the hgvs is a snv
     if line.startswith('<td class="clsf">'):
         clsf = re.search('clsf">(.*)[<]', line).group(1)
         clsf = clsf[0]
