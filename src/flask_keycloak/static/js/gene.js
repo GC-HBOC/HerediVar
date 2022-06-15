ddba220c5e9375a382a5b3c4fa34dba30953dd9e
MarvinDo <qarst@gmx.de>
2022-06-15 16:26:58 +0200

    updated keycloak tests

--- src/flask_keycloak/static/js/gene.js
+++
@@ -1,5 +0,0 @@
-table = document.getElementById("transcriptTable");
-if (table != null) {
-    filterTable_one_column("ensembl", 4, table);
-    sorter(['#transcripts_numflags_col', '#transcripts_length_col']) // sort first by num of flags and if there is a tie sort by length
-}
