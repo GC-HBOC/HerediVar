95258b8a71bdaee464e3ba692e5ddeb088ddd56e
MarvinDo <qarst@gmx.de>
2022-07-26 14:45:27 +0200

    added database dump script & first dump

---
+++ src/tools/database_dumper/dump_database.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+set -o pipefail
+set -o verbose
+
+#-P 3306 -h SRV011.img.med.uni-tuebingen.de 
+#mysqldump -P 3306 -h SRV011.img.med.uni-tuebingen.de -u ahdoebm1 -p HerediVar_ahdoebm1 > db_backup.sql
+DATE=$(date '+%F');
+path_to_dump=./dumps/dump-$DATE.sql
+
+mysqldump --quick HerediVar_ahdoebm1 -P 3306 -h SRV011.img.med.uni-tuebingen.de -u ahdoebm1 --no-tablespaces -r $path_to_dump -p
+gzip $path_to_dump
