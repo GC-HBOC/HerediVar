9157fa5e54bda555a1e4e4fb43177069abaaa5ab
MarvinDo <qarst@gmx.de>
2022-08-31 15:36:41 +0200

    update ci

---
+++ src/frontend_celery/keycloak_export/export_keycloak.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd $SCRIPTPATH
+
+keycloak_path=/mnt/users/ahdoebm1/HerediVar/src/tools/keycloak-18.0.0/bin
+
+$keycloak_path/kc.sh export --file Heredivar-realm.json --realm HerediVar
+
