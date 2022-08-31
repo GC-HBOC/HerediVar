250c655f05f472337b2e26b9f9e50a5354565295
MarvinDo <qarst@gmx.de>
2022-08-31 13:09:38 +0200

    update init

--- src/frontend_celery/tests/script/init_keycoak.sh
+++ src/frontend_celery/tests/script/init_keycoak.sh
@@ -3,8 +3,10 @@
 #!/bin/bash
 
 
-keycloak-18.0.0/bin/kcadm.sh config credentials --server http://localhost:5050/auth --realm master --user admin --client admin
+#keycloak-18.0.0/bin/kcadm.sh config credentials --server http://localhost:5050/auth --realm master --user admin --client admin
 
-keycloak-18.0.0/bin/kcadm.sh create realms -f src/frontend_celery/keycloak_export/HerediVar-realm.json
+bin/kc.sh import --file src/frontend_celery/keycloak_export/HerediVar-realm.json
+
+#keycloak-18.0.0/bin/kcadm.sh create realms -f src/frontend_celery/keycloak_export/HerediVar-realm.json
 
 keycloak-18.0.0/bin/kcadm.sh get realms
