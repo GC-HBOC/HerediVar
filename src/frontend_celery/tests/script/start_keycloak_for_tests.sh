37463cd2d3c323848fc99e07afc8157fe98a7968
MarvinDo <qarst@gmx.de>
2022-08-31 12:06:45 +0200

    update start keycloak

--- src/frontend_celery/tests/script/start_keycloak_for_tests.sh
+++ src/frontend_celery/tests/script/start_keycloak_for_tests.sh
@@ -1,6 +1,29 @@
 
 #!/bin/bash
 
+function waitForServer {
+  dist=$1
+  echo -n "Starting $dist"
+  # Give the server some time to start up. Look for a well-known
+  # bit of text in the log file. Try at most 50 times before giving up.
+  C=50
+  while :
+  do
+    grep "$dist .* (powered by Quarkus .*) started" keycloak.log
+    if [ $? -eq 0 ]; then
+      echo " server started."
+      break
+    elif [ $C -gt 0 ]; then
+      echo -n "."
+      C=$((C-1))
+      sleep 1
+    else
+      echo " timeout!"
+      cat keycloak.log
+      exit 1
+    fi
+  done
+}
 
-
-keycloak-18.0.0/bin/kc.sh start-dev --hostname localhost --http-port 5050 --features=admin-fine-grained-authz # --log-level debug
+keycloak-18.0.0/bin/kc.sh start-dev --hostname localhost --http-port 5050 --features=admin-fine-grained-authz  > keycloak.log 2>&1 & # --log-level debug
+waitForServer "Keycloak"
