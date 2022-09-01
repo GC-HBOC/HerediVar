471c70b3807df0d42c6d487f22778b811a93dc03
MarvinDo <qarst@gmx.de>
2022-09-01 10:04:22 +0200

    update keycloak realms

--- src/frontend_celery/keycloak_export/export_keycloak.sh
+++ src/frontend_celery/keycloak_export/export_keycloak.sh
@@ -9,3 +9,4 @@ keycloak_path=/mnt/users/ahdoebm1/HerediVar/src/tools/keycloak-18.0.0/bin
 
 $keycloak_path/kc.sh export --file Heredivar-realm.json --realm HerediVar
 
+sed 's/"directAccessGrantsEnabled" : false,/"directAccessGrantsEnabled" : true,/g' Heredivar-realm.json > Heredivar-realm-test.json
--- src/frontend_celery/tests/script/prepare_keycloak.sh
+++ src/frontend_celery/tests/script/prepare_keycloak.sh
@@ -6,4 +6,4 @@ unzip keycloak-18.0.0.zip
 rm keycloak-18.0.0.zip
 
 # initialize keycloak
-keycloak-18.0.0/bin/kc.sh import --file src/frontend_celery/keycloak_export/Heredivar-realm.json
+keycloak-18.0.0/bin/kc.sh import --file src/frontend_celery/keycloak_export/Heredivar-realm-test.json
