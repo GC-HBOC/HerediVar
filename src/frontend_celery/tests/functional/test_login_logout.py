9b13bddbf5764e17077174623decdfda589da7e2
MarvinDo <qarst@gmx.de>
2022-09-01 10:14:33 +0200

    update login tests

--- src/frontend_celery/tests/functional/test_login_logout.py
+++ src/frontend_celery/tests/functional/test_login_logout.py
@@ -7,11 +7,11 @@ def test_login(test_client):
     """
     DOCSTRING
     """
-    response = test_client.get("/login", follow_redirects=True)
+    response = test_client.get("/create", follow_redirects=True)
     print(request.path)
-    print(response)
+    print(response.json())
     print(session['tokenResponse'])
-    assert response.status_code == 2300
+    assert response.status_code == 200
     #keycloak_auth_url = response.location
     #print(keycloak_auth_url)
     
