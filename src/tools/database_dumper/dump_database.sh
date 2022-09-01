936afe2dc4b33224193e63e2c2d6ed3d4f690f2e
MarvinDo <qarst@gmx.de>
2022-09-01 15:36:53 +0200

    update database dump & ci

--- src/tools/database_dumper/dump_database.sh
+++ src/tools/database_dumper/dump_database.sh
@@ -10,3 +10,6 @@ path_to_dump=./dumps/dump-$DATE.sql
 
 mysqldump --quick HerediVar_ahdoebm1 -P 3306 -h SRV011.img.med.uni-tuebingen.de -u ahdoebm1 --no-tablespaces -r $path_to_dump -p
 gzip $path_to_dump
+
+
+echo dumps/dump-$DATE.sql > most_recent_dump.txt
