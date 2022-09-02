7f935b42b1c7cb06565a4679ec9717f40343d2c2
MarvinDo <qarst@gmx.de>
2022-09-02 09:06:02 +0200

    update ci

--- src/tools/database_dumper/dump_database.sh
+++ src/tools/database_dumper/dump_database.sh
@@ -6,10 +6,17 @@ set -o verbose
 #-P 3306 -h SRV011.img.med.uni-tuebingen.de 
 #mysqldump -P 3306 -h SRV011.img.med.uni-tuebingen.de -u ahdoebm1 -p HerediVar_ahdoebm1 > db_backup.sql
 DATE=$(date '+%F');
-path_to_dump=./dumps/dump-$DATE.sql
+#path_to_dump=./dumps/dump-$DATE.sql
+path_to_structure=./structure/structure-$DATE.sql
+path_to_data=./data/data-$DATE.sql
 
-mysqldump --quick HerediVar_ahdoebm1 -P 3306 -h SRV011.img.med.uni-tuebingen.de -u ahdoebm1 --no-tablespaces -r $path_to_dump -p
-gzip $path_to_dump
+mysqldump --quick HerediVar_ahdoebm1 -P 3306 -h SRV011.img.med.uni-tuebingen.de -u ahdoebm1 --no-tablespaces --no-data -r $path_to_structure -p
+gzip $path_to_structure
 
 
-echo dumps/dump-$DATE.sql > most_recent_dump.txt
+mysqldump --quick HerediVar_ahdoebm1 -P 3306 -h SRV011.img.med.uni-tuebingen.de -u ahdoebm1 --no-tablespaces --no-create-info -r $path_to_data -p
+gzip $path_to_data
+
+
+
+echo $DATE > most_recent_dump.txt
