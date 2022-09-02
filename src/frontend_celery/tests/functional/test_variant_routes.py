53f01d34d627847ca41a9b3c71db95c56ad7f48b
MarvinDo <qarst@gmx.de>
2022-09-02 14:41:11 +0200

    readout browse test

--- src/frontend_celery/tests/functional/test_variant_routes.py
+++ src/frontend_celery/tests/functional/test_variant_routes.py
@@ -36,6 +36,7 @@ def test_browse(test_client):
     This tests if the browse variant table works properly
     """
     response = test_client.get(url_for("variant.search"), follow_redirects=True)
+    print(response.data)
     assert response.status_code == 2000
     
     
