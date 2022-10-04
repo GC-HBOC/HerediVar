30448bec0d809518023916b1b765ac2dd0d05a4e
MarvinDo <qarst@gmx.de>
2022-10-04 13:08:00 +0200

    update ci

--- src/annotation_service/tests/run_tests.sh
+++ src/annotation_service/tests/run_tests.sh
@@ -2,9 +2,11 @@
 #!/bin/bash
 
 SCRIPT=$(readlink -f "$0")
+echo $SCRIPT
 SCRIPTPATH=$(dirname "$SCRIPT")
+echo $SCRIPTPATH
 SCRIPTPATH=$(dirname "$SCRIPTPATH")
-SCRIPTPATH=$(dirname "$SCRIPTPATH")
+echo $SCRIPTPATH
 cd $SCRIPTPATH
 
 python -m pytest ##-v
