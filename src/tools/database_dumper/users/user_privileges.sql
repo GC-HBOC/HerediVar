41454ac8d1df43d61f5b9ef805a3296d6fc9f875
MarvinDo <qarst@gmx.de>
2022-11-04 09:17:14 +0100

    update user privileges

--- src/tools/database_dumper/users/user_privileges.sql
+++ src/tools/database_dumper/users/user_privileges.sql
@@ -8,50 +8,52 @@ CREATE USER 'HerediVar_annotation'@'%' IDENTIFIED BY '20220303a' PASSWORD EXPIRE
 CREATE USER 'HerediVar_user'@'%' IDENTIFIED BY '20220303u' PASSWORD EXPIRE NEVER;
 */
 
+
+
 /* grant priviliges to annotation user */
-GRANT SELECT, INSERT, UPDATE ON HerediVar_ahdoebm1.variant_annotation TO 'HerediVar_annotation';
-GRANT SELECT, INSERT ON HerediVar_ahdoebm1.variant_literature TO 'HerediVar_annotation';
-GRANT SELECT, INSERT ON HerediVar_ahdoebm1.variant_consequence TO 'HerediVar_annotation';
-GRANT SELECT ON HerediVar_ahdoebm1.annotation_type TO 'HerediVar_annotation';
-GRANT SELECT ON HerediVar_ahdoebm1.gene TO 'HerediVar_annotation';
-GRANT SELECT ON HerediVar_ahdoebm1.transcript TO 'HerediVar_annotation';
-GRANT SELECT, INSERT, DELETE ON HerediVar_ahdoebm1.variant TO 'HerediVar_annotation';
-GRANT SELECT ON HerediVar_ahdoebm1.task_force_protein_domains TO 'HerediVar_annotation';
-GRANT SELECT, UPDATE ON HerediVar_ahdoebm1.annotation_queue TO 'HerediVar_annotation';
-GRANT SELECT, INSERT, DELETE ON HerediVar_ahdoebm1.clinvar_variant_annotation TO 'HerediVar_annotation';
-GRANT INSERT, DELETE ON HerediVar_ahdoebm1.clinvar_submission TO 'HerediVar_annotation';
-GRANT INSERT ON HerediVar_ahdoebm1.heredicare_center_classification TO 'HerediVar_annotation';
-GRANT INSERT, DELETE ON HerediVar_ahdoebm1.variant_ids TO 'HerediVar_annotation';
-GRANT SELECT ON HerediVar_ahdoebm1.pfam_id_mapping TO 'HerediVar_annotation';
-GRANT SELECT ON HerediVar_ahdoebm1.pfam_legacy TO 'HerediVar_annotation';
+GRANT SELECT, INSERT, UPDATE ON HerediVar.variant_annotation TO 'HerediVar_annotation';
+GRANT SELECT, INSERT ON HerediVar.variant_literature TO 'HerediVar_annotation';
+GRANT SELECT, INSERT ON HerediVar.variant_consequence TO 'HerediVar_annotation';
+GRANT SELECT ON HerediVar.annotation_type TO 'HerediVar_annotation';
+GRANT SELECT ON HerediVar.gene TO 'HerediVar_annotation';
+GRANT SELECT ON HerediVar.transcript TO 'HerediVar_annotation';
+GRANT SELECT, INSERT, DELETE ON HerediVar.variant TO 'HerediVar_annotation';
+GRANT SELECT ON HerediVar.task_force_protein_domains TO 'HerediVar_annotation';
+GRANT SELECT, UPDATE ON HerediVar.annotation_queue TO 'HerediVar_annotation';
+GRANT SELECT, INSERT, DELETE ON HerediVar.clinvar_variant_annotation TO 'HerediVar_annotation';
+GRANT INSERT, DELETE ON HerediVar.clinvar_submission TO 'HerediVar_annotation';
+GRANT INSERT ON HerediVar.heredicare_center_classification TO 'HerediVar_annotation';
+GRANT INSERT, DELETE ON HerediVar.variant_ids TO 'HerediVar_annotation';
+GRANT SELECT ON HerediVar.pfam_id_mapping TO 'HerediVar_annotation';
+GRANT SELECT ON HerediVar.pfam_legacy TO 'HerediVar_annotation';
 
 /* grant priviliges to standard user */
-GRANT SELECT, INSERT, UPDATE ON HerediVar_ahdoebm1.user TO 'HerediVar_user';
-GRANT SELECT ON HerediVar_ahdoebm1.* TO 'HerediVar_user';
-GRANT INSERT ON HerediVar_ahdoebm1.assay TO 'HerediVar_user';
-GRANT INSERT, DELETE ON HerediVar_ahdoebm1.list_variants TO 'HerediVar_user';
-GRANT INSERT, UPDATE ON HerediVar_ahdoebm1.user_classification TO 'HerediVar_user';
-GRANT INSERT, UPDATE, DELETE ON HerediVar_ahdoebm1.user_classification_criteria_applied TO 'HerediVar_user';
-GRANT INSERT, UPDATE, DELETE ON HerediVar_ahdoebm1.user_variant_lists TO 'HerediVar_user';
-GRANT INSERT ON HerediVar_ahdoebm1.variant TO 'HerediVar_user';
-GRANT INSERT, UPDATE ON HerediVar_ahdoebm1.annotation_queue TO 'HerediVar_user';
+GRANT SELECT, INSERT, UPDATE ON HerediVar.user TO 'HerediVar_user';
+GRANT SELECT ON HerediVar.* TO 'HerediVar_user';
+GRANT INSERT ON HerediVar.assay TO 'HerediVar_user';
+GRANT INSERT, DELETE ON HerediVar.list_variants TO 'HerediVar_user';
+GRANT INSERT, UPDATE ON HerediVar.user_classification TO 'HerediVar_user';
+GRANT INSERT, UPDATE, DELETE ON HerediVar.user_classification_criteria_applied TO 'HerediVar_user';
+GRANT INSERT, UPDATE, DELETE ON HerediVar.user_variant_lists TO 'HerediVar_user';
+GRANT INSERT ON HerediVar.variant TO 'HerediVar_user';
+GRANT INSERT, UPDATE ON HerediVar.annotation_queue TO 'HerediVar_user';
 
 
 /* grant priviliges to super user */
-GRANT SELECT, INSERT, UPDATE ON HerediVar_ahdoebm1.user TO 'HerediVar_superuser';
-GRANT SELECT ON HerediVar_ahdoebm1.* TO 'HerediVar_superuser';
-GRANT INSERT ON HerediVar_ahdoebm1.assay TO 'HerediVar_superuser';
-GRANT INSERT, DELETE ON HerediVar_ahdoebm1.list_variants TO 'HerediVar_superuser';
-GRANT INSERT, UPDATE ON HerediVar_ahdoebm1.user_classification TO 'HerediVar_superuser';
-GRANT INSERT, UPDATE, DELETE ON HerediVar_ahdoebm1.user_classification_criteria_applied TO 'HerediVar_superuser';
-GRANT INSERT, UPDATE, DELETE ON HerediVar_ahdoebm1.user_variant_lists TO 'HerediVar_superuser';
-GRANT INSERT ON HerediVar_ahdoebm1.variant TO 'HerediVar_superuser';
-GRANT INSERT, UPDATE ON HerediVar_ahdoebm1.annotation_queue TO 'HerediVar_superuser';
+GRANT SELECT, INSERT, UPDATE ON HerediVar.user TO 'HerediVar_superuser';
+GRANT SELECT ON HerediVar.* TO 'HerediVar_superuser';
+GRANT INSERT ON HerediVar.assay TO 'HerediVar_superuser';
+GRANT INSERT, DELETE ON HerediVar.list_variants TO 'HerediVar_superuser';
+GRANT INSERT, UPDATE ON HerediVar.user_classification TO 'HerediVar_superuser';
+GRANT INSERT, UPDATE, DELETE ON HerediVar.user_classification_criteria_applied TO 'HerediVar_superuser';
+GRANT INSERT, UPDATE, DELETE ON HerediVar.user_variant_lists TO 'HerediVar_superuser';
+GRANT INSERT ON HerediVar.variant TO 'HerediVar_superuser';
+GRANT INSERT, UPDATE ON HerediVar.annotation_queue TO 'HerediVar_superuser';
 
-GRANT INSERT, UPDATE ON HerediVar_ahdoebm1.variant_ids TO 'HerediVar_superuser';
-GRANT INSERT, UPDATE ON HerediVar_ahdoebm1.consensus_classification TO 'HerediVar_superuser';
-GRANT INSERT ON HerediVar_ahdoebm1.consensus_classification_criteria_applied TO 'HerediVar_superuser';
-GRANT INSERT ON HerediVar_ahdoebm1.import_queue TO 'HerediVar_superuser';
+GRANT INSERT, UPDATE ON HerediVar.variant_ids TO 'HerediVar_superuser';
+GRANT INSERT, UPDATE ON HerediVar.consensus_classification TO 'HerediVar_superuser';
+GRANT INSERT ON HerediVar.consensus_classification_criteria_applied TO 'HerediVar_superuser';
+GRANT INSERT ON HerediVar.import_queue TO 'HerediVar_superuser';
 
 
 
