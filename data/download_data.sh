dfb3d68d4be26417c380a3b7b6f27e5d3b8ac8bd
MarvinDo <qarst@gmx.de>
2023-03-03 08:55:14 +0100

    update download data

--- data/download_data.sh
+++ data/download_data.sh
@@ -48,119 +48,99 @@ genome=$data/genomes/GRCh38.fa
 mkdir -p $dbs
 
 # download GRCH38 genome
-cd $data
-chmod 755 download_GRCh38.sh
-./download_GRCh38.sh
+#cd $data
+#chmod 755 download_GRCh38.sh
+#./download_GRCh38.sh
 
 
 # download GRCH37 reference_genome (used for lifting)
-cd $data
-chmod 755 download_GRCh37.sh
-./download_GRCh37.sh
+#cd $data
+#chmod 755 download_GRCh37.sh
+#./download_GRCh37.sh
 
 
 # download HGNC
-cd $dbs
-mkdir -p HGNC
-cd HGNC
-wget -O - http://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt > hgnc_complete_set.tsv
+#cd $dbs
+#mkdir -p HGNC
+#cd HGNC
+#wget -O - http://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt > hgnc_complete_set.tsv
 
 
 
 
 # download BRCA exchange (https://brcaexchange.org/releases)
-
-cd $dbs
-mkdir -p BRCA_exchange
-cd BRCA_exchange
-wget https://brcaexchange.org/backend/downloads/releases/release-02-22-22/release-02-22-22.tar.gz
-tar -xf release-02-22-22.tar.gz
-mv -f output/release/built_with_change_types.tsv .
-rm -rf output/
-python3 $tools/db_converter_brca_exchange.py -i built_with_change_types.tsv | $ngsbits/VcfLeftNormalize -stream -ref $genome | $ngsbits/VcfStreamSort | bgzip > BRCA_exchange_02-22-22.vcf.gz
-tabix -p vcf BRCA_exchange_02-22-22.vcf.gz
-
-
-: '
-# download ensembl transcripts (http://ftp.ensembl.org/pub/current_gff3/homo_sapiens/)
-cd $dbs
-mkdir -p ensembl
-cd ensembl
-wget http://ftp.ensembl.org/pub/current_gff3/homo_sapiens/Homo_sapiens.GRCh38.105.gff3.gz
-gunzip Homo_sapiens.GRCh38.105.gff3.gz
-## download ensembl canonical transcripts (http://ftp.ensembl.org/pub/current_tsv/homo_sapiens)
-wget -O - http://ftp.ensembl.org/pub/release-105/tsv/homo_sapiens/Homo_sapiens.GRCh38.105.canonical.tsv.gz | gunzip > Homo_sapiens.GRCh38.105.canonical.tsv
-'
-
-: '
-# download mane select (https://ftp.ncbi.nlm.nih.gov/refseq/MANE/)
-cd $dbs
-mkdir -p MANE
-cd MANE
-wget -O - https://ftp.ncbi.nlm.nih.gov/refseq/MANE/MANE_human/release_1.0/MANE.GRCh38.v1.0.ensembl_genomic.gff.gz | gunzip > MANE.GRCh38.v1.0.ensembl_genomic.gff
-'
+#cd $dbs
+#mkdir -p BRCA_exchange
+#cd BRCA_exchange
+#wget https://brcaexchange.org/backend/downloads/releases/release-02-22-22/release-02-22-22.tar.gz
+#tar -xf release-02-22-22.tar.gz
+#mv -f output/release/built_with_change_types.tsv .
+#rm -rf output/
+#python3 $tools/db_converter_brca_exchange.py -i built_with_change_types.tsv | $ngsbits/VcfLeftNormalize -stream -ref $genome | $ngsbits/VcfStreamSort | bgzip > BRCA_exchange_02-22-22.vcf.gz
+#tabix -p vcf BRCA_exchange_02-22-22.vcf.gz
 
 
 
 
-# download FLOSSIES (https://whi.color.com/about) there is no versioning so the date specified in the filename equals the date when the database was accessed
-cd $dbs
-mkdir -p FLOSSIES
-cd FLOSSIES
-flossies_file=FLOSSIES_25-03-2022.vcf
-cat $data/FLOSSIES_data_uris.txt | python3 $tools/data_uri_to_blob.py --header | python3 $tools/db_converter_flossies.py > $flossies_file
-$ngsbits/VcfSort -in $flossies_file -out $flossies_file
-$ngsbits/VcfLeftNormalize -in $flossies_file -stream -ref $data/genomes/GRCh37.fa -out $flossies_file.2
-$ngsbits/VcfStreamSort -in $flossies_file.2 -out $flossies_file
-awk -v OFS="\t" '!/##/ {$9=$10=""}1' $flossies_file |sed 's/^\s\+//g' > $flossies_file.2 # remove SAMPLE and FORMAT columns from vcf as they are added by vcfsort
-mv -f $flossies_file.2 $flossies_file
-bgzip $flossies_file
 
-$ngsbits/VcfCheck -in $flossies_file.gz -ref $data/genomes/GRCh37.fa
 
-# crossmap to lift from GRCh37 to GRCh37
-CrossMap.py vcf $data/genomes/hg19ToHg38.fixed.over.chain.gz $flossies_file.gz $genome $flossies_file.2
-cat $flossies_file.2 | $ngsbits/VcfLeftNormalize -stream -ref $genome | $ngsbits/VcfStreamSort | bgzip > $flossies_file.gz
-tabix -p vcf $flossies_file.gz
-rm -f $flossies_file.2
+## download FLOSSIES (https://whi.color.com/about) there is no versioning so the date specified in the filename equals the date when the database was accessed
+#cd $dbs
+#mkdir -p FLOSSIES
+#cd FLOSSIES
+#flossies_file=FLOSSIES_25-03-2022.vcf
+#cat $data/FLOSSIES_data_uris.txt | python3 $tools/data_uri_to_blob.py --header | python3 $tools/db_converter_flossies.py > $flossies_file
+#$ngsbits/VcfSort -in $flossies_file -out $flossies_file
+#$ngsbits/VcfLeftNormalize -in $flossies_file -stream -ref $data/genomes/GRCh37.fa -out $flossies_file.2
+#$ngsbits/VcfStreamSort -in $flossies_file.2 -out $flossies_file
+#awk -v OFS="\t" '!/##/ {$9=$10=""}1' $flossies_file |sed 's/^\s\+//g' > $flossies_file.2 # remove SAMPLE and FORMAT columns from vcf as they are added by vcfsort
+#mv -f $flossies_file.2 $flossies_file
+#bgzip $flossies_file
+#$ngsbits/VcfCheck -in $flossies_file.gz -ref $data/genomes/GRCh37.fa
 
-$ngsbits/VcfCheck -in $flossies_file.gz -ref $genome
+## crossmap to lift from GRCh37 to GRCh37
+#CrossMap.py vcf $data/genomes/hg19ToHg38.fixed.over.chain.gz $flossies_file.gz $genome $flossies_file.2
+#cat $flossies_file.2 | $ngsbits/VcfLeftNormalize -stream -ref $genome | $ngsbits/VcfStreamSort | bgzip > $flossies_file.gz
+#tabix -p vcf $flossies_file.gz
+#rm -f $flossies_file.2
+#$ngsbits/VcfCheck -in $flossies_file.gz -ref $genome
 
 
 
 # download dbSNP
-cd $dbs
-mkdir -p dbSNP
-cd dbSNP
-wget -O - https://ftp.ncbi.nih.gov/snp/archive/b155/VCF/GCF_000001405.39.gz | gunzip | python3 $tools/vcf_refseq_to_chrnum.py | $ngsbits/VcfBreakMulti | $ngsbits/VcfLeftNormalize -stream -ref $genome | $ngsbits/VcfStreamSort > dbSNP_v155.vcf
-bgzip dbSNP_v155.vcf
-tabix -p vcf dbSNP_v155.vcf.gz
+#cd $dbs
+#mkdir -p dbSNP
+#cd dbSNP
+#wget -O - https://ftp.ncbi.nih.gov/snp/archive/b155/VCF/GCF_000001405.39.gz | gunzip | python3 $tools/vcf_refseq_to_chrnum.py | $ngsbits/VcfBreakMulti | $ngsbits/VcfLeftNormalize -stream -ref $genome | $ngsbits/VcfStreamSort > dbSNP_v155.vcf
+#bgzip dbSNP_v155.vcf
+#tabix -p vcf dbSNP_v155.vcf.gz
 
 
 
 # download phyloP conservation scores (https://www.ensembl.org/info/docs/tools/vep/script/vep_example.html#gerp)
-cd $dbs
-mkdir -p phyloP
-cd phyloP
-wget http://hgdownload.soe.ucsc.edu/goldenPath/hg38/phyloP100way/hg38.phyloP100way.bw
+#cd $dbs
+#mkdir -p phyloP
+#cd phyloP
+#wget http://hgdownload.soe.ucsc.edu/goldenPath/hg38/phyloP100way/hg38.phyloP100way.bw
 
 
 
 # Install CADD - http://cadd.gs.washington.edu/download
-cd $dbs
-mkdir -p CADD
-cd CADD
-wget -O - http://kircherlab.bihealth.org/download/CADD/v1.6/GRCh38/whole_genome_SNVs.tsv.gz > CADD_SNVs_1.6_GRCh38.tsv.gz
-zcat CADD_SNVs_1.6_GRCh38.tsv.gz | python3 $tools/db_converter_cadd.py | $ngsbits/VcfLeftNormalize -stream -ref $genome | $ngsbits/VcfStreamSort | bgzip > CADD_SNVs_1.6_GRCh38.vcf.gz
-tabix -f -p vcf CADD_SNVs_1.6_GRCh38.vcf.gz
-rm -f CADD_SNVs_1.6_GRCh38.tsv.gz
-$ngsbits/VcfCheck -in CADD_SNVs_1.6_GRCh38.vcf.gz -ref $genome -lines 0
+#cd $dbs
+#mkdir -p CADD
+#cd CADD
+#wget -O - http://kircherlab.bihealth.org/download/CADD/v1.6/GRCh38/whole_genome_SNVs.tsv.gz > CADD_SNVs_1.6_GRCh38.tsv.gz
+#zcat CADD_SNVs_1.6_GRCh38.tsv.gz | python3 $tools/db_converter_cadd.py | $ngsbits/VcfLeftNormalize -stream -ref $genome | $ngsbits/VcfStreamSort | bgzip > CADD_SNVs_1.6_GRCh38.vcf.gz
+#tabix -f -p vcf CADD_SNVs_1.6_GRCh38.vcf.gz
+#rm -f CADD_SNVs_1.6_GRCh38.tsv.gz
+$ngsbits/VcfCheck -in CADD_SNVs_1.6_GRCh38.vcf.gz -ref $genome #-lines 0
+
 
 wget -O - https://kircherlab.bihealth.org/download/CADD/v1.6/GRCh38/gnomad.genomes.r3.0.indel.tsv.gz > CADD_InDels_1.6_GRCh38.tsv.gz
 zcat CADD_InDels_1.6_GRCh38.tsv.gz | python3 $tools/db_converter_cadd.py | $ngsbits/VcfLeftNormalize -stream -ref $genome | $ngsbits/VcfStreamSort | bgzip > CADD_InDels_1.6_GRCh38.vcf.gz
 tabix -f -p vcf CADD_InDels_1.6_GRCh38.vcf.gz
 rm -f CADD_InDels_1.6_GRCh38.tsv.gz
-$ngsbits/VcfCheck -in CADD_InDels_1.6_GRCh38.vcf.gz -ref $genome -lines 0
+$ngsbits/VcfCheck -in CADD_InDels_1.6_GRCh38.vcf.gz -ref $genome #-lines 0
 
 
 
@@ -179,14 +159,7 @@ rm -f revel_tmp.tsv.gz h revel_grch38_all_chromosomes.tsv
 $ngsbits/VcfCheck -in revel_grch38_all_chromosomes.vcf.gz -ref $genome -lines 0
 
 
-: '
-# download annotation file for SpliceAI
-cd $dbs
-mkdir -p SpliceAI
-cd SpliceAI
-wget https://download.imgag.de/ahsturm1/spliceai_scores_2022_02_09_GRCh38.vcf.gz
-tabix -p vcf spliceai_scores_2022_02_09_GRCh38.vcf.gz
-'
+
 
 
 # download ClinVar (https://www.ncbi.nlm.nih.gov/clinvar/)
@@ -211,15 +184,6 @@ tabix -p vcf clinvar_20220320_converted_GRCh38.vcf.gz
 wget -O - http://ftp.ncbi.nlm.nih.gov/pub/clinvar/tab_delimited/archive/variant_summary_2021-12.txt.gz | gunzip > variant_summary_2021-12.txt
 cat variant_summary_2021-12.txt | php $src/Tools/db_converter_clinvar_cnvs.php 5 "Pathogenic/Likely pathogenic" | sort | uniq > clinvar_cnvs_2021-12.bed
 $ngsbits/BedSort -with_name -in clinvar_cnvs_2021-12.bed -out clinvar_cnvs_2021-12.bed
-'
-
-: '
-## download PFAM (last accessed at realease 35.0)
-cd $dbs
-mkdir -p PFAM
-cd PFAM
-wget -O - http://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.seed.gz | zcat | iconv -f utf-8 -t utf-8 -c | python3 $tools/db_converter_pfam.py > pfam_id_mapping.tsv
-wget -O - ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.dead.gz | zcat | iconv -f utf-8 -t utf-8 -c | python3 $tools/db_converter_pfam.py --deadfile > pfam_legacy.tsv
 '
 
 
@@ -263,33 +227,6 @@ $ngsbits/VcfCheck -in $cancerhotspotsfile.final.vcf.gz -ref $genome
 
 tabix -p vcf $cancerhotspotsfile.final.vcf.gz
 
-
-: '
-# download refseq transcripts release 110
-cd $dbs
-mkdir -p RefSeq
-cd RefSeq
-
-wget -O - https://ftp.ncbi.nlm.nih.gov/genomes/refseq/vertebrate_mammalian/Homo_sapiens/annotation_releases/110/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.gff.gz > refseq_transcripts_110.gff.gz
-'
-
-: '
-# download ensembl refseq transcript id mapping table
-cd $dbs
-mkdir -p mapping_tables
-cd mapping_tables
-wget https://github.com/imgag/ngs-bits/raw/master/src/cppNGS/Resources/hg38_ensembl_transcript_matches.tsv
-'
-
-
-: '
-## download OMIM mapping table (downloaded 02.05.2022)
-cd $dbs
-mkdir -p OMIM
-cd OMIM
-
-wget https://www.omim.org/static/omim/data/mim2gene.txt
-'
 
 
 # download orphanet mapping table (downloaded 02.05.2022)
@@ -421,6 +358,80 @@ rm priors_hg19.vcf.gz.tbi
 
 
 
+#### SPECIAL DATA
+: '
+# download annotation file for SpliceAI
+cd $dbs
+mkdir -p SpliceAI
+cd SpliceAI
+wget https://download.imgag.de/ahsturm1/spliceai_scores_2022_02_09_GRCh38.vcf.gz
+tabix -p vcf spliceai_scores_2022_02_09_GRCh38.vcf.gz
+'
+
+
+
+
+
+
+
+
+#### DATA FOR INIT DB
+: '
+# download ensembl transcripts (http://ftp.ensembl.org/pub/current_gff3/homo_sapiens/)
+cd $dbs
+mkdir -p ensembl
+cd ensembl
+wget http://ftp.ensembl.org/pub/current_gff3/homo_sapiens/Homo_sapiens.GRCh38.105.gff3.gz
+gunzip Homo_sapiens.GRCh38.105.gff3.gz
+## download ensembl canonical transcripts (http://ftp.ensembl.org/pub/current_tsv/homo_sapiens)
+wget -O - http://ftp.ensembl.org/pub/release-105/tsv/homo_sapiens/Homo_sapiens.GRCh38.105.canonical.tsv.gz | gunzip > Homo_sapiens.GRCh38.105.canonical.tsv
+'
+
+: '
+# download mane select (https://ftp.ncbi.nlm.nih.gov/refseq/MANE/)
+cd $dbs
+mkdir -p MANE
+cd MANE
+wget -O - https://ftp.ncbi.nlm.nih.gov/refseq/MANE/MANE_human/release_1.0/MANE.GRCh38.v1.0.ensembl_genomic.gff.gz | gunzip > MANE.GRCh38.v1.0.ensembl_genomic.gff
+'
+
+
+
+: '
+## download PFAM (last accessed at realease 35.0)
+cd $dbs
+mkdir -p PFAM
+cd PFAM
+wget -O - http://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.seed.gz | zcat | iconv -f utf-8 -t utf-8 -c | python3 $tools/db_converter_pfam.py > pfam_id_mapping.tsv
+wget -O - ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.dead.gz | zcat | iconv -f utf-8 -t utf-8 -c | python3 $tools/db_converter_pfam.py --deadfile > pfam_legacy.tsv
+'
+
+: '
+# download refseq transcripts release 110
+cd $dbs
+mkdir -p RefSeq
+cd RefSeq
+
+wget -O - https://ftp.ncbi.nlm.nih.gov/genomes/refseq/vertebrate_mammalian/Homo_sapiens/annotation_releases/110/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.gff.gz > refseq_transcripts_110.gff.gz
+'
+
+: '
+# download ensembl refseq transcript id mapping table
+cd $dbs
+mkdir -p mapping_tables
+cd mapping_tables
+wget https://github.com/imgag/ngs-bits/raw/master/src/cppNGS/Resources/hg38_ensembl_transcript_matches.tsv
+'
+
+
+: '
+## download OMIM mapping table (downloaded 02.05.2022)
+cd $dbs
+mkdir -p OMIM
+cd OMIM
+
+wget https://www.omim.org/static/omim/data/mim2gene.txt
+'
 
 
 
