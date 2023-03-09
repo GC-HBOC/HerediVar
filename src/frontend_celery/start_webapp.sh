90c64083cc1c00553a72e012275db77e702289f7
MarvinDo <qarst@gmx.de>
2023-03-09 08:58:59 +0100

    added some config examples & update start webapp

--- src/frontend_celery/start_webapp.sh
+++ src/frontend_celery/start_webapp.sh
@@ -1,10 +1,48 @@
 #!/bin/bash
+set -e
+set -o pipefail
+
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -w env"
+   echo "This script starts the heredivar frontend gunicorn or development server"
+   echo -e "\t-w Provide 'dev' for development server and 'prod' for production gunicorn server."
+   exit 1 # Exit script after printing help
+}
+
+while getopts "w:" opt
+do
+   case "$opt" in
+      w ) we="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$we" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+
+
+
 
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd $SCRIPTPATH
+cd ../../
+pwd
 
-echo "${WEBAPP_ENV}"
+export WEBAPP_ENV=$we
+
+source .venv/bin/activate
+cd $SCRIPTPATH
+
+echo starting Heredivar in "${WEBAPP_ENV}" mode
 
 if [ -z "${WEBAPP_ENV}" ]
 then
