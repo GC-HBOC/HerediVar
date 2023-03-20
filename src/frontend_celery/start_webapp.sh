9477e57077a41f1982ff3c70a06b96001192c660
MarvinDo <qarst@gmx.de>
2023-03-20 15:34:17 +0100

    added environment variable

--- src/frontend_celery/start_webapp.sh
+++ src/frontend_celery/start_webapp.sh
@@ -46,17 +46,18 @@ echo starting Heredivar in "${WEBAPP_ENV}" mode
 
 if [ -z "${WEBAPP_ENV}" ]
 then
-    echo Environment variable WEBAPP_ENV not set.
-    exit 1
+   echo Environment variable WEBAPP_ENV not set.
+   exit 1
 fi
 
 if [ "${WEBAPP_ENV}" == "dev" ]
 then
-    python3 main.py
+   python3 main.py
 fi
 
 if [ "${WEBAPP_ENV}" == "prod" ]
 then
-    gunicorn -b heredivar.uni-koeln.de:8000 -w 4 'webapp:create_app()'
-    #gunicorn -b SRV018.img.med.uni-tuebingen.de:8001 -w 1 'webapp:create_app()'
+   export CURL_CA_BUNDLE=""
+   gunicorn -b heredivar.uni-koeln.de:8000 -w 4 'webapp:create_app()'
+   #gunicorn -b SRV018.img.med.uni-tuebingen.de:8001 -w 1 'webapp:create_app()'
 fi
