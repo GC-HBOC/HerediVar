73bd631965cc399b67f65f12d9e8f14bb2497d93
MarvinDo <qarst@gmx.de>
2023-03-20 14:15:00 +0100

    better start redis script

--- src/frontend_celery/start_redis.sh
+++ src/frontend_celery/start_redis.sh
@@ -1,17 +1,58 @@
 #!/bin/bash
 
 
-root="$(dirname `pwd`)"
-tools=$root/tools/
 
-cd $tools
+#!/bin/bash
+set -e
+set -o pipefail
 
 
-if [ ! -d redis-stable/src ]; then
-    curl -O http://download.redis.io/redis-stable.tar.gz
-    tar xvzf redis-stable.tar.gz
-    rm redis-stable.tar.gz
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -w env"
+   echo "This script starts the heredivar frontend gunicorn or development server"
+   echo -e "\t-w Provide 'dev' for development server and 'prod' for production gunicorn server."
+   exit 1 # Exit script after printing help
+}
+
+while getopts "w:" opt
+do
+   case "$opt" in
+      w ) we="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$we" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
 fi
-cd redis-stable
-make
+
+echo "preparing redis startup"
+
+
+
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd $SCRIPTPATH
+cd ../../
+pwd
+
+
+
+source .venv/bin/activate
+export WEBAPP_ENV=$we
+
+if [ -z "${WEBAPP_ENV}" ]
+then
+    echo Environment variable WEBAPP_ENV not set.
+    exit 1
+fi
+
+
+cd src/tools/redis-stable
 src/redis-server
