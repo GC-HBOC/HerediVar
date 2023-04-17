9069642ca1cf3d3ccd24c4024e0b8eeb876058a6
MarvinDo <qarst@gmx.de>
2023-04-17 14:39:07 +0200

    small fix

--- src/frontend_celery/webapp/io/download_routes.py
+++ src/frontend_celery/webapp/io/download_routes.py
@@ -405,12 +405,11 @@ def ref_genome_index():
 
 @download_blueprint.route('/download/gnomad.vcf.gz')
 def gnomad():
-    filename = "gnomad.vcf.gz"
-    return send_from_directory(path=paths.gnomad_path, download_name="gnomAD_genome_v3.1.2_GRCh38.vcf.gz", as_attachment=True, mimetype="text")
+    filename = "gnomAD_genome_v3.1.2_GRCh38.vcf.gz"
+    return send_from_directory(directory=paths.datadir + 'gnomAD/', path=filename, download_name="gnomAD_genome_v3.1.2_GRCh38.vcf.gz", as_attachment=True, mimetype="text")
 
 
 @download_blueprint.route('/download/gnomad_m.vcf.gz')
 def gnomad_m():
-    filename = "gnomad_m.vcf.gz"
-    flash(paths.datadir + 'gnomAD/')
-    return send_from_directory(directory=paths.gnomad_m_path + 'gnomAD/', path=filename, download_name="gnomAD_genome_v3.1.mito_GRCh38.vcf.gz", as_attachment=True, mimetype="text")
+    filename = "gnomAD_genome_v3.1.mito_GRCh38.vcf.gz"
+    return send_from_directory(directory=paths.datadir + 'gnomAD/', path=filename, download_name="gnomAD_genome_v3.1.mito_GRCh38.vcf.gz", as_attachment=True, mimetype="text")
