e5cd7d5dd7ffa5d68628f614526310949517b138
MarvinDo <qarst@gmx.de>
2023-11-20 09:17:22 +0100

    update update database

--- resources/backups/database_dumper/update_database_2.sql
+++ resources/backups/database_dumper/update_database_2.sql
@@ -26,53 +26,53 @@
 -- ADD UNIQUE INDEX `UNIQUE_variant_transcript_annotation` (`variant_id` ASC, `annotation_type_id` ASC, `transcript` ASC),
 -- DROP INDEX `id_UNIQUE` ;
 
-ALTER TABLE `HerediVar`.`annotation_type` 
-ADD COLUMN `is_transcript_specific` TINYINT(1) NOT NULL DEFAULT 0 AFTER `group_name`;
+-- ALTER TABLE `HerediVar`.`annotation_type` 
+-- ADD COLUMN `is_transcript_specific` TINYINT(1) NOT NULL DEFAULT 0 AFTER `group_name`;
+-- 
+-- INSERT INTO `HerediVar`.`annotation_type` (`title`, `display_title`, `description`, `value_type`, `version`, `version_date`, `group_name`, `is_transcript_specific`) VALUES ('maxentscan', 'MaxEntScan', 'The transcript specific MaxEntScan scores calculated from ngs-bits. Each value is of the form: ref|alt', 'text', 'v1.0.0', '2023-09-27', 'Splicing', 1);
+-- INSERT INTO `HerediVar`.`annotation_type` (`title`, `display_title`, `description`, `value_type`, `version`, `version_date`, `group_name`, `is_transcript_specific`) VALUES ('maxentscan_swa', 'MaxEntScan SWA', 'The transcript specific MaxEntScan SWA scores calculated from ngs-bits. A special application of the MaxEntScan algorithm to discover de-novo spliceing variants. Each value is of the form maxentscan_ref_donor|maxentscan_alt_donor|maxentscan_donor_comp|maxentscan_ref_acceptor|maxentscan_alt_acceptor|maxentscan_acceptor_comp', 'text', 'v1.0.0', '2023-09-27', 'Splicing', 1);
+-- 
+-- 
+-- 
+-- 
+-- GRANT SELECT, INSERT, UPDATE ON HerediVar.variant_transcript_annotation TO 'HerediVar_annotation';
+-- GRANT SELECT ON HerediVar.variant_transcript_annotation TO 'HerediVar_user';
+-- GRANT SELECT ON HerediVar.variant_transcript_annotation TO 'HerediVar_superuser';
+-- GRANT SELECT ON HerediVar.variant_transcript_annotation TO 'HerediVar_read_only';
+-- 
+-- 
+-- 
+-- 
+-- UPDATE `HerediVar`.`annotation_type` SET `group_name` = 'Protein Domain' WHERE (`id` = '36');
+-- UPDATE `HerediVar`.`annotation_type` SET `group_name` = 'Protein Domain' WHERE (`id` = '37');
+-- UPDATE `HerediVar`.`annotation_type` SET `group_name` = 'None' WHERE (`id` = '9');
+-- UPDATE `HerediVar`.`annotation_type` SET `group_name` = 'None' WHERE (`id` = '10');
+-- 
+-- 
+-- 
+-- 
+-- ALTER TABLE `HerediVar`.`variant_heredicare_annotation` 
+-- ADD INDEX `FK_variant_heredicare_annotation_variant_idx` (`variant_id` ASC);
+-- ;
+-- ALTER TABLE `HerediVar`.`variant_heredicare_annotation` 
+-- ADD CONSTRAINT `FK_variant_heredicare_annotation_variant`
+--   FOREIGN KEY (`variant_id`)
+--   REFERENCES `HerediVar`.`variant` (`id`)
+--   ON DELETE NO ACTION
+--   ON UPDATE NO ACTION;
+-- 
+-- 
+-- ALTER TABLE `HerediVar`.`annotation_queue` 
+-- CHANGE COLUMN `error_message` `error_message` TEXT NULL DEFAULT '' ;
+-- 
+-- 
+-- ALTER TABLE `HerediVar`.`variant_ids` 
+-- ADD COLUMN `annotation_type_id` INT UNSIGNED NOT NULL AFTER `id_source`;
+-- 
+-- 
+-- INSERT INTO `HerediVar`.`annotation_type` (`title`, `display_title`, `description`, `value_type`, `version`, `version_date`, `group_name`, `is_transcript_specific`) VALUES ('heredicare_vid', 'HerediCare VID', 'The VID from HerediCare.The version_date is inaccurate. They are always up to date when reimporting from heredicare.', 'int', '-', '2023-01-01', 'ID', '0');
 
-INSERT INTO `HerediVar`.`annotation_type` (`title`, `display_title`, `description`, `value_type`, `version`, `version_date`, `group_name`, `is_transcript_specific`) VALUES ('maxentscan', 'MaxEntScan', 'The transcript specific MaxEntScan scores calculated from ngs-bits. Each value is of the form: ref|alt', 'text', 'v1.0.0', '2023-09-27', 'Splicing', 1);
-INSERT INTO `HerediVar`.`annotation_type` (`title`, `display_title`, `description`, `value_type`, `version`, `version_date`, `group_name`, `is_transcript_specific`) VALUES ('maxentscan_swa', 'MaxEntScan SWA', 'The transcript specific MaxEntScan SWA scores calculated from ngs-bits. A special application of the MaxEntScan algorithm to discover de-novo spliceing variants. Each value is of the form maxentscan_ref_donor|maxentscan_alt_donor|maxentscan_donor_comp|maxentscan_ref_acceptor|maxentscan_alt_acceptor|maxentscan_acceptor_comp', 'text', 'v1.0.0', '2023-09-27', 'Splicing', 1);
-
-
-
-
-GRANT SELECT, INSERT, UPDATE ON HerediVar.variant_transcript_annotation TO 'HerediVar_annotation';
-GRANT SELECT ON HerediVar.variant_transcript_annotation TO 'HerediVar_user';
-GRANT SELECT ON HerediVar.variant_transcript_annotation TO 'HerediVar_superuser';
-GRANT SELECT ON HerediVar.variant_transcript_annotation TO 'HerediVar_read_only';
-
-
-
-
-UPDATE `HerediVar`.`annotation_type` SET `group_name` = 'Protein Domain' WHERE (`id` = '36');
-UPDATE `HerediVar`.`annotation_type` SET `group_name` = 'Protein Domain' WHERE (`id` = '37');
-UPDATE `HerediVar`.`annotation_type` SET `group_name` = 'None' WHERE (`id` = '9');
-UPDATE `HerediVar`.`annotation_type` SET `group_name` = 'None' WHERE (`id` = '10');
-
-
-
-
-ALTER TABLE `HerediVar`.`variant_heredicare_annotation` 
-ADD INDEX `FK_variant_heredicare_annotation_variant_idx` (`variant_id` ASC);
-;
-ALTER TABLE `HerediVar`.`variant_heredicare_annotation` 
-ADD CONSTRAINT `FK_variant_heredicare_annotation_variant`
-  FOREIGN KEY (`variant_id`)
-  REFERENCES `HerediVar`.`variant` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-
-ALTER TABLE `HerediVar`.`annotation_queue` 
-CHANGE COLUMN `error_message` `error_message` TEXT NULL DEFAULT '' ;
-
-
-ALTER TABLE `HerediVar`.`variant_ids` 
-ADD COLUMN `annotation_type_id` INT UNSIGNED NOT NULL AFTER `id_source`;
-
-
-INSERT INTO `HerediVar`.`annotation_type` (`title`, `display_title`, `description`, `value_type`, `version`, `version_date`, `group_name`, `is_transcript_specific`) VALUES ('heredicare_vid', 'HerediCare VID', 'The VID from HerediCare.The version_date is inaccurate. They are always up to date when reimporting from heredicare.', 'int', '-', '2023-01-01', 'ID', '0');
-
-UPDATE variant_ids SET annotation_type_id = (SELECT id FROM annotation_type WHERE title = 'heredicare_vid') WHERE id_source = 'heredicare'
+UPDATE variant_ids SET annotation_type_id = (SELECT id FROM annotation_type WHERE title = 'heredicare_vid') WHERE id_source = 'heredicare';
 
 
 ALTER TABLE `HerediVar`.`variant_ids` 
