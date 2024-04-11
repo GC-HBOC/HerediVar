19a52a331d12fd50c407218948954ba6373e9617
MarvinDo <qarst@gmx.de>
2024-04-11 15:19:32 +0200

    update update database

--- resources/backups/database_dumper/update_database_8.sql
+++ resources/backups/database_dumper/update_database_8.sql
@@ -1,9 +1,9 @@
-ALTER TABLE `HerediVar_ahdoebm1`.`import_variant_queue` 
+ALTER TABLE `HerediVar`.`import_variant_queue` 
 CHANGE COLUMN `vid` `vid` TEXT NULL ;
 
 
 
-CREATE TABLE `HerediVar_ahdoebm1`.`list_variant_import_queue` (
+CREATE TABLE `HerediVar`.`list_variant_import_queue` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `list_id` INT NOT NULL,
   `user_id` INT NOT NULL,
@@ -17,17 +17,17 @@ CREATE TABLE `HerediVar_ahdoebm1`.`list_variant_import_queue` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-ALTER TABLE `HerediVar_ahdoebm1`.`list_variant_import_queue` 
+ALTER TABLE `HerediVar`.`list_variant_import_queue` 
 CHANGE COLUMN `requested_at` `requested_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ;
 
 
-GRANT SELECT,INSERT,UPDATE ON HerediVar_ahdoebm1.list_variant_import_queue TO 'HerediVar_user';
-GRANT SELECT,INSERT,UPDATE ON HerediVar_ahdoebm1.list_variant_import_queue TO 'HerediVar_superuser';
-GRANT SELECT,INSERT,UPDATE ON HerediVar_ahdoebm1.list_variant_import_queue TO 'HerediVar_read_only';
+GRANT SELECT,INSERT,UPDATE ON HerediVar.list_variant_import_queue TO 'HerediVar_user';
+GRANT SELECT,INSERT,UPDATE ON HerediVar.list_variant_import_queue TO 'HerediVar_superuser';
+GRANT SELECT,INSERT,UPDATE ON HerediVar.list_variant_import_queue TO 'HerediVar_read_only';
 
-ALTER TABLE `HerediVar_ahdoebm1`.`list_variant_import_queue` 
+ALTER TABLE `HerediVar`.`list_variant_import_queue` 
 CHANGE COLUMN `status` `status` ENUM('progress', 'pending', 'success', 'error', 'retry', 'aborted') NOT NULL DEFAULT 'pending' ;
 
 
-ALTER TABLE `HerediVar_ahdoebm1`.`import_queue` 
+ALTER TABLE `HerediVar`.`import_queue` 
 ADD COLUMN `source` ENUM('heredicare', 'vcf') NOT NULL DEFAULT 'heredicare' AFTER `celery_task_id`;
