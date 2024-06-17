0066f5db8cbf89630a9cb0a34da0a12d67d54b53
MarvinDo <qarst@gmx.de>
2024-06-17 08:40:13 +0200

    update update database

--- resources/update_database/update_database_10.sql
+++ resources/update_database/update_database_10.sql
@@ -1,9 +1,9 @@
-ALTER TABLE `HerediVar_ahdoebm1`.`classification_scheme_alias` 
+ALTER TABLE `HerediVar`.`classification_scheme_alias` 
 ADD COLUMN `version` VARCHAR(45) NOT NULL AFTER `alias`;
 
-ALTER TABLE `HerediVar_ahdoebm1`.`classification_scheme_alias` 
+ALTER TABLE `HerediVar`.`classification_scheme_alias` 
 ADD UNIQUE INDEX `classification_scheme_alias_unique` (`alias` ASC, `version` ASC);
 ;
 
-ALTER TABLE `HerediVar_ahdoebm1`.`user` 
+ALTER TABLE `HerediVar`.`user` 
 ADD COLUMN `api_key` VARCHAR(64) NULL AFTER `affiliation`;
