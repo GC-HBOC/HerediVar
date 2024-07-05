3c76d4450fd1736f4965bd821500f5ea653aa4f3
MarvinDo <qarst@gmx.de>
2024-07-05 15:23:00 +0200

    update update database

--- resources/update_database/update_database_11.sql
+++ resources/update_database/update_database_11.sql
@@ -14,3 +14,5 @@ SET FOREIGN_KEY_CHECKS = 1;
 DELETE FROM variant_annotation WHERE annotation_type_id IN (
 SELECT id FROM annotation_type WHERE title IN ("cancerhotspots", "cancerhotspots_ac", "cancerhotspots_af")
 );
+
+DELETE FROM variant_ids WHERE annotation_type_id = (SELECT id FROM annotation_type WHERE display_title = 'cosmic-id')
