99e1be4a91c1bf2ae5c9d7eec4d35a33e9648f9d
MarvinDo <qarst@gmx.de>
2024-07-05 16:05:57 +0200

    update changelog and update database

--- resources/update_database/update_database_11.sql
+++ resources/update_database/update_database_11.sql
@@ -1,9 +1,9 @@
-ALTER TABLE `HerediVar_ahdoebm1`.`import_queue` 
+ALTER TABLE `HerediVar`.`import_queue` 
 CHANGE COLUMN `source` `source` ENUM('heredicare', 'vcf', 'heredicare_complete', 'heredicare_specific') NOT NULL DEFAULT 'heredicare' ;
 
 UPDATE import_queue SET source = 'heredicare_complete' WHERE source = 'heredicare';
 
-ALTER TABLE `HerediVar_ahdoebm1`.`import_queue` 
+ALTER TABLE `HerediVar`.`import_queue` 
 CHANGE COLUMN `source` `source` ENUM('vcf', 'heredicare_complete', 'heredicare_specific') NOT NULL DEFAULT 'heredicare_complete' ;
 
 
