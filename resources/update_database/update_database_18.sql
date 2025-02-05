b71c50fe558aa61ed32c712ef15180c8406dd3fb
MarvinDo <qarst@gmx.de>
2025-02-05 17:19:22 +0100

    fixed a bug when annotating R, 4M or 5M variants

---
+++ resources/update_database/update_database_18.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `HerediVar`.`heredicare_center_classification` 
+CHANGE COLUMN `classification` `classification` ENUM('1', '2', '3', '11', '12', '32', '13', '34', '14', '15', '20', '21', '4', '16', '17', '18') NOT NULL ;
